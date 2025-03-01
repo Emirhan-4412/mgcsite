64b771bf7c1501846359a68bd77a75891672a5dc
Emirhan-4412 <166597676+Emirhan-4412@users.noreply.github.com>
2025-03-01 16:40:41 +0300

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import Link from 'next/link';
-import styles from '../styles/Header.module.css';
+import styles from '../styles/Header.module.css'; // CSS modülünü import et
 
 export default function Header() {
   const [isAdmin, setIsAdmin] = useState(false);
