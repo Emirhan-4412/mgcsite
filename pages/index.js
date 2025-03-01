481bd57173f82a12160294216199512bd1601858
Emirhan-4412 <166597676+Emirhan-4412@users.noreply.github.com>
2025-03-01 16:11:33 +0300

    index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Merhaba, GitHub ve Vercel!</h1>
+      <p>Bu proje GitHub üzerinden deploy edildi.</p>
+    </div>
+  );
+}
